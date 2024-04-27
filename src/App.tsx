bfb799a4e3e75ce7f415a56cc4a46f587cad10ad
Daneryl <daneryl@gmail.com>
2024-04-27 17:00:50 +0200

    WIP, First basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { fetchJurisdictions, fetchSubJurisdictions } from "./fake_api";
 
 function App() {
+  const [jurisdictions, setJurisdictions] = useState([]);
+  const [subJurisdictions, setSubJurisdictions] = useState({});
+
+  useEffect(() => {
+    fetchJurisdictions().then((response) => {
+      setJurisdictions(response);
+    });
+  }, []);
+
+  if (!jurisdictions.length) {
+    return (
+      <>
+        <div>Loading ...</div>
+      </>
+    );
+  }
+
   return (
     <>
-      <div>Loading ...</div>
+      {jurisdictions.map((jurisdiction) => {
+        return (
+          <div
+            onClick={() => {
+              setSubJurisdictions({ ...subJurisdictions, ...{ 1: [] } });
+              fetchSubJurisdictions(jurisdiction.id).then(
+                (subjurisdictions) => {
+                  setSubJurisdictions({
+                    ...subJurisdictions,
+                    ...{ 1: subjurisdictions },
+                  });
+                }
+              );
+            }}
+          >
+            {jurisdiction.name}
+            {subJurisdictions[jurisdiction.id] &&
+              !subJurisdictions[jurisdiction.id].length && (
+                <div>Loading {jurisdiction.name} ...</div>
+              )}
+
+            {subJurisdictions[jurisdiction.id] &&
+              subJurisdictions[jurisdiction.id].length &&
+              subJurisdictions[jurisdiction.id].map((subjurisdiction) => {
+                return <div>{subjurisdiction.name}</div>;
+              })}
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/specs/App.spec.tsx
+++ src/specs/App.spec.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "../App";
 
 describe("App", () => {
@@ -9,12 +9,31 @@ describe("App", () => {
     expect(screen.getByText("Loading ...")).toBeVisible();
   });
 
-  it("should load jurisdictions and display them as checkboxes", async () => {
+  it("should load jurisdictions display them and remove Loading state", async () => {
     render(<App />);
     expect(screen.getByText("Loading ...")).toBeVisible();
     await waitFor(() => {
-      expect(screen.getByText("SPAIN")).toBeVisible();
+      expect(screen.getByText("Spain")).toBeVisible();
+      expect(screen.getByText("United Kingdom")).toBeVisible();
     });
-    expect(screen.getByText("Loading ...")).toBeNull();
+    expect(screen.queryByText("Loading ...")).toBeNull();
+  });
+
+  it("should load subjurisdictions when selecting one jurisdiction", async () => {
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getByText("Spain")).toBeVisible();
+    });
+    fireEvent.click(screen.getByText("Spain"));
+    expect(screen.getByText("Loading Spain ...")).toBeVisible();
+    expect(screen.queryByText("Loading United Kingdom ...")).toBeNull();
+
+    await waitFor(
+      () => {
+        expect(screen.getByText("Aragón")).toBeVisible();
+      },
+      { timeout: 2000 }
+    );
+    expect(screen.queryByText("Loading Spain ...")).toBeNull();
   });
 });
