5bf0d2764fe732eeacc279b3780e869b40aea46d
Daneryl <daneryl@gmail.com>
2024-04-27 16:12:05 +0200

    First test

--- src/specs/App.spec.tsx
+++ src/specs/App.spec.tsx
@@ -1,11 +1,20 @@
 import { describe, it, expect } from "vitest";
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "../App";
 
 describe("App", () => {
-  it("renders headline", () => {
+  it("should display a loading state", () => {
     render(<App />);
-    expect(screen.getByText('Vite + React')).toBeVisible();
+    expect(screen.getByText("Loading ...")).toBeVisible();
+  });
+
+  it("should load jurisdictions and display them as checkboxes", async () => {
+    render(<App />);
+    expect(screen.getByText("Loading ...")).toBeVisible();
+    await waitFor(() => {
+      expect(screen.getByText("SPAIN")).toBeVisible();
+    });
+    expect(screen.getByText("Loading ...")).toBeNull();
   });
 });
